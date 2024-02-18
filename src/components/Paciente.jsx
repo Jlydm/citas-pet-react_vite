5d56fd1d208b08bd91dbf9ecf5306d53314d9a85
Jlydm <juliancito8790@gmail.com>
2024-02-17 21:08:08 -0500

    first commit

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,28 @@
+const Paciente = () => {
+  return (
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+        
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+            <span className="font-normal normal-case">Julisn</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+            <span className="font-normal normal-case">correo@correo.com</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+            <span className="font-normal normal-case">10 de Diciembre de 2024</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+            <span className="font-normal normal-case">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, consequatur, magnam atque, minima laboriosam possimus veniam sunt harum laborum eveniet libero dolorum! Fugiat deleniti nostrum officiis doloremque veritatis soluta iusto.</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+
+    </div>
+  )
+}
+
+export default Paciente
