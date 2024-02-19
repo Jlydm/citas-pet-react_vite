0f5d0b181d2fe87e66cc0cfabd81490f140e86eb
Jlydm <juliancito8790@gmail.com>
2024-02-18 19:28:49 -0500

    Primer y espero ultimo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
+import { useState, useEffect } from "react" 
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
+  const [pacientes, setPacientes] = useState([]);
+  const [paciente, setPaciente] = useState({});
+
+  // Detecta algo en storage y lo carga 
+  useEffect(() => {
+    const obtenerLS = () => {
+        const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+        setPacientes(pacientesLS)
+    }
+
+    obtenerLS();
+  }, [])
+
+  // Hace que tenga almacenamiento los pacientes que se tienen
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify( pacientes ))
+  }, [pacientes])
+
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id)
+    setPacientes(pacientesActualizados)
+  }
+
+  const toma1valor = (valor) => {
+    console.log(valor)
+  }
+
   return (
-    <div className="container mx-auto mt-20">
+    <div className="container mx-auto mt-2">
       <Header />
       <div className="mt-12 md:flex">
-        <Formulario />
-        <ListadoPacientes />
+        <Formulario 
+          setPacientes={setPacientes}
+          pacientes={pacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes 
+          pacientes={pacientes}
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
       </div>
     </div>
   )
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,76 @@
 import { useState, useEffect } from "react";
+import Error from "./Error";
 
-const Formulario = () => {
+const Formulario = ({ setPacientes, pacientes, paciente, setPaciente }) => {
     const [nombre, setNombre] = useState('');
     const [propietario, setPropietario] = useState('');
     const [email, setEmail] = useState('');
     const [fecha, setFecha] = useState('');
     const [sintomas, setSintomas] = useState('');
 
-    const handleSubmit = () =>{
-        
+    const [error, setError] = useState(false);
+
+    // cargan losprops que ya estan en paciente
+    useEffect(() => {
+        if( Object.keys(paciente).length > 0 ) {
+            setNombre(paciente.nombre)
+            setPropietario(paciente.propietario)
+            setEmail(paciente.email)
+            setFecha(paciente.fecha)
+            setSintomas(paciente.sintomas)
+        }
+    }, [paciente])
+
+    // Genera un id
+    const generarID = () => {
+        const random = Math.random().toString(36).substr(2);
+        const fecha = Date.now().toString(36);
+        return random + fecha
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        //Validaciones del Formulario
+        if([ nombre, propietario, email, fecha, sintomas ].includes('')){
+            setError(true)
+            return;
+        }
+        setError(false);
+
+        // Objeto de Paciente
+        const objetoPaciente = {
+            nombre, 
+            propietario, 
+            email, 
+            fecha, 
+            sintomas
+        }
+
+        if(paciente.id){
+            // Editando registro
+            // Le asigna el id ya existente al paciente 
+            objetoPaciente.id = paciente.id
+
+            // Mira el paciente que tenga el mismo id que la edición y le retorna el nuevo con los cambios
+            const pacientesActualizados = pacientes.map( pacienteState => pacienteState.id === paciente.id ? objetoPaciente : pacienteState )
+
+            setPacientes(pacientesActualizados)
+            setPaciente({})
+        }else{
+            // Nuevo registro
+            // Si no tiene el id se lo agrega
+            objetoPaciente.id = generarID();
+            // Toma una copia del registro vacio y le agrega el nuevo paciente
+            setPacientes([...pacientes, objetoPaciente])  
+        }
+
+        // Reiniciar el form
+        setNombre('')
+        setPropietario('')
+        setEmail('')
+        setFecha('')
+        setSintomas('')
     }
 
     return (
@@ -23,6 +85,7 @@ const Formulario = () => {
             <form
                 onSubmit={handleSubmit} 
                 className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+                { error && <Error><p>Todos los campos son obligatorios</p></Error>}
                <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
                         Nombre Mascota
@@ -89,7 +152,7 @@ const Formulario = () => {
                 <input 
                     type="submit"
                     className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
-                    value="Agregar Paciente"
+                    value={ paciente.id ? 'Editar paciente' : 'Agregar paciente'}
                 />
             </form>
         </div>    
